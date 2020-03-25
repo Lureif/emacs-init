e5e85a286da1f196aadfda6d9cd5e4cc1fadf3ca
Lureif <pyramid_dragon@protonmail.com>
2020-03-25 10:24:54 +0100

    Refactored init file.

--- init.el
+++ init.el
@@ -1,30 +1,41 @@
+;;; init.el --- My Emacs init file
+;;; --------------------------------------------------
+;;; Commentary:
+;;; This is file is constantly evolving.  What you see now may not be what I currently use.
+;;; --------------------------------------------------
+;;; Code:
+
 (package-initialize)
-;; melpa
+
+;;; melpa
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list
    'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t))
 
+;;; custom-set-variables
 (setq custom-file (concat user-emacs-directory "/custom.el"))
 (load-file custom-file)
 
+;;; use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (require 'use-package)
-
 (setq use-package-always-ensure t)
 
-;; overall settings
+;;; Disabling the default shit
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (toggle-scroll-bar -1)
 
-;; (global-display-line-numbers-mode)
-(setq c-default-style "linux")
+;;(global-display-line-numbers-mode nil)
+(defvar c-default-style "linux")
+(defvar inferior-lisp-program "sbcl")
+(setq inhibit-startup-message t
+      initial-buffer-choice 'eshell)
 
-;; themes
+;;; Themes
 (load-theme 'dracula t)		;; dark purple (a little cringe)
 ;;(load-theme 'gruvbox-dark-hard t)	;; go-to dark theme
 ;;(load-theme 'base16-classic-dark)	;; yet another tango-dark style theme
@@ -36,12 +47,25 @@
 ;;(load-theme 'base16-icy)		;; dark blue
 ;;(load-theme 'base16-cupcake)		;; purple-ish white (v good)
 
+
+;;; Movement stuff
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "C-c C-c") 'comment-line)
+(global-set-key (kbd "C-x C-j") 'ace-jump-mode)
+(global-set-key (kbd "C-x C-l") 'ace-jump-line-mode)
+(global-set-key (kbd "C-x o") 'ace-window)
+(global-set-key (kbd "C-c C-e C-b") 'eval-buffer)
+(global-set-key (kbd "M-$") 'query-replace)
+(global-unset-key (kbd "C-; \?t"))
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;; Plugins
+
 (use-package powerline
   :ensure t
   :config (powerline-default-theme))
 
-;;;; bindings
-;; HELM
+;;; Helm
 (use-package helm
   :ensure t
   :bind (("C-x C-f"	. 'helm-find-files)
@@ -50,7 +74,7 @@
 	 ("C-x b"	. 'helm-mini)
 	 ("M-x"		. 'helm-M-x)))
 
-;; MAGIT
+;;; Magit
 (use-package magit
   :ensure t
   :bind (("C-; C-d" . 'magit-diff)
@@ -62,25 +86,6 @@
 	 ("C-; C-u" . 'magit-pull)
 	 ("C-; C-i" . 'magit-init)
 	 ("C-; C-m" . 'magit-merge)))
-
-;;;; Movement/Convenience
-(global-set-key (kbd "M-;") 'comment-or-uncomment-region)
-(global-set-key (kbd "C-c C-c") 'comment-line)
-(global-set-key (kbd "C-x C-j") 'ace-jump-mode)
-(global-set-key (kbd "C-x C-l") 'ace-jump-line-mode)
-(global-set-key (kbd "C-x o") 'ace-window)
-;; (global-set-key (kbd "C-<left>") 'windmove-left)
-;; (global-set-key (kbd "C-<right>") 'windmove-right)
-;; (global-set-key (kbd "C-<up>") 'windmove-up)
-;; (global-set-key (kbd "C-<down>") 'windmove-down)
-(global-set-key (kbd "C-c C-e C-b") 'eval-buffer)
-(global-set-key (kbd "M-$") 'query-replace)
-(global-unset-key (kbd "C-; \?t"))
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; plugins and such
-(ido-mode t)
-(setq inferior-lisp-program "sbcl")
 
 (use-package company
   :ensure t
@@ -102,7 +107,8 @@
 	     (yas-global-mode t)
 	     (yas-reload-all))
 
-;; ORG
+;;; ORG
+;; vanilla stuff
 (use-package org
   :ensure t
   :bind (("C-c l" . 'org-store-link)
@@ -115,20 +121,21 @@
 			   "~/.org/home.org"
 			   "~/.org/personal_projects.org"
 			   "~/.org/self_progress.org"
-			   "~/.org/thoughts.org"))
-  ;; (setq org-agenda-file-regexp "\\`\\\([^.].*\\.org\\\|[0-9]\\\{8\\\}\\\(\\.gpg\\\)?\\\)\\'")
-  ;; (add-to-list 'org-agenda-files org-journal-dir)
-  (use-package org-journal
-    :bind (("C-c j" . 'org-journal-new-entry)
-	   ("C-c C-f" . 'org-journal-next-entry)
-	   ("C-c C-b" . 'org-journal-previous-entry)
-	   ("C-c C-s" . 'org-journal-search))
-    :config
-    (setq org-journal-dir "~/.org/journal")))
-;; start up setup
-(setq inhibit-startup-message t
-      initial-buffer-choice 'eshell)
+			   "~/.org/thoughts.org")))
+;; journal
+(use-package org-journal
+  :ensure t
+  :bind (("C-c j" . 'org-journal-new-entry)
+	 ("C-c C-f" . 'org-journal-next-entry)
+	 ("C-c C-b" . 'org-journal-previous-entry)
+	 ("C-c C-s" . 'org-journal-search))
+  :config
+  (setq org-journal-dir "~/.org/journal"))
 
 ;;;; transparency!
 ;; (set-frame-parameter (selected-frame) 'alpha '(85 85))
-;; (add-to-list 'default-frame-alist '(alpha 85 85))
+;; (add-to-list 'default-frame-alist '(alpha 85 85)
+
+;;;--------------------------------------------------
+(provide 'init)
+;;; init.el ends here
